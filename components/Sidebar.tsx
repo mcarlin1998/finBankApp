cdf720f3ce8bc3b4b813fd5331898baf531c220a
Michael Carlin <michaelcarlin076@gmail.com>
2024-05-07 16:56:33 +0700

    Sidebar layout UI adjustments to include nav

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -10,7 +10,7 @@ const Sidebar = ({ user }: SiderbarProps) => {
   return (
     <section className="sidebar">
       <nav className="flex flex-col gap-4">
-        <Link href="/" className="mb-12 cursor-pointer items-center gap-2">
+        <Link href="/" className="mb-12 cursor-pointer flex items-center gap-2">
           <Image
             src="/icons/logo.svg"
             width={34}
@@ -21,13 +21,25 @@ const Sidebar = ({ user }: SiderbarProps) => {
           <h1 className="sidebar-logo">Fis</h1>
         </Link>
         {sidebarLinks.map((item) => {
+          const isActive =
+            pathName === item.route || pathName.startsWith(`${item.route}/`);
           return (
             <Link
               href={item.route}
               key={item.label}
-              className={cn("sidebar-link")}
+              className={cn("sidebar-link", { "bg-bank-gradient": isActive })}
             >
-              {item.label}
+              <div className="relative size-6">
+                <Image
+                  src={item.imgURL}
+                  alt={item.label}
+                  fill
+                  className={cn({ "brightness-[3] invert-0": isActive })}
+                />
+              </div>
+              <p className={cn("sidebar-label", { "!text-white": isActive })}>
+                {item.label}
+              </p>
             </Link>
           );
         })}
