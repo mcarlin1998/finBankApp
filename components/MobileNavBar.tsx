71b7afc91fe0697b00c293fff60c8039437d91b9
Michael Carlin <michaelcarlin076@gmail.com>
2024-05-13 12:55:18 +0700

    Mobile Nav Sidbar development and resposiveiness

--- components/MobileNavBar.tsx
+++ components/MobileNavBar.tsx
@@ -1,14 +1,22 @@
+"use client";
 import {
   Sheet,
+  SheetClose,
   SheetContent,
   SheetDescription,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import { cn } from "@/lib/utils";
 import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { sidebarLinks } from "@/constants";
 
 const MobileNavBar = ({ user }: MobileNavProps) => {
+  const pathName = usePathname();
+
   return (
     <section className="w-full max-w-[245px]">
       <Sheet>
@@ -21,14 +29,62 @@ const MobileNavBar = ({ user }: MobileNavProps) => {
             className="cursor-pointer"
           />
         </SheetTrigger>
-        <SheetContent>
-          <SheetHeader>
-            <SheetTitle>Are you absolutely sure?</SheetTitle>
-            <SheetDescription>
-              This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
-            </SheetDescription>
-          </SheetHeader>
+        <SheetContent side="left" className="border-none bg-white">
+          <Link
+            href="/"
+            className=" cursor-pointer flex items-center gap-1 px-4"
+          >
+            <Image
+              src="/icons/logo.svg"
+              width={34}
+              height={34}
+              alt="Fis Logo"
+            />
+            <h1 className="text-26 font-ibm-plex-serif font-bold text-black-1">
+              Fis
+            </h1>
+          </Link>
+          <div className="mobilenav-sheet">
+            <SheetClose asChild>
+              <nav className="flex h-full flex-col gap-6 pt-16 text-white">
+                {sidebarLinks.map((item) => {
+                  const isActive =
+                    pathName === item.route ||
+                    pathName.startsWith(`${item.route}/`);
+                  return (
+                    <SheetClose asChild key={item.route}>
+                      <Link
+                        href={item.route}
+                        key={item.label}
+                        className={cn("mobilenav-sheet_close w-full", {
+                          "bg-bank-gradient": isActive,
+                        })}
+                      >
+                        <div className="relative size-6">
+                          <Image
+                            src={item.imgURL}
+                            alt={item.label}
+                            width={20}
+                            height={20}
+                            className={cn({
+                              "brightness-[3] invert-0": isActive,
+                            })}
+                          />
+                        </div>
+                        <p
+                          className={cn("text-16 font-semibold text-black-2", {
+                            "!text-white": isActive,
+                          })}
+                        >
+                          {item.label}
+                        </p>
+                      </Link>
+                    </SheetClose>
+                  );
+                })}
+              </nav>
+            </SheetClose>
+          </div>
         </SheetContent>
       </Sheet>
     </section>
